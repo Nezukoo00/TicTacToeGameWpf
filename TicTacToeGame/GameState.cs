cdddbd4bbb27b2a1b6ccfdf8ff56cd9fb1c1fb7d
Nezukoo00 <viktoruapogiba@gmail.com>
2024-05-24 12:44:53 +0300

    new functions

--- TicTacToeGame/GameState.cs
+++ TicTacToeGame/GameState.cs
@@ -35,18 +35,48 @@ namespace TicTacToeGame
         }
         private void SwitchPlayer ()
         {
-            if (CurrentPlayer == Player.X)
-            {
-                CurrentPlayer == Player.O;
-            }
-            else
-            {
-                CurrentPlayer == Player.X;
-            }
+            CurrentPlayer = CurrentPlayer == Player.X ? Player.O : Player.X;
         }
         private bool AreSquaresMarked((int,int)[] squares, Player player)
         {
-            foreach ()
+            foreach ((int r, int c) in squares)
+            {
+                if (GameGrid[r, c] != player)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private bool DidMoveWin (int r, int c, out WinInfo winInfo)
+        {
+            (int, int)[] row = new[] { (r,0),(r,1),(r,2)};
+            (int, int)[] col = new[] { ( 0,c), (1,c), (2,c) };
+            (int, int)[] mainDiag = new[] { (0,0), (1,1), (2,2) };
+            (int, int)[] antiDiag = new[] { (0,2), (1,1), (2,0) };
+
+            if (AreSquaresMarked(row,CurrentPlayer))
+            {
+                winInfo = new WinInfo {Type = WinType.Row, Number  = r };
+                return true;
+            }
+            if (AreSquaresMarked(col,CurrentPlayer)) { winInfo = new WinInfo { Type = WinType.Column, Number = c}; return true; }
+            if (AreSquaresMarked(mainDiag, CurrentPlayer))
+            {
+                winInfo = new WinInfo { Type = WinType.MainDiagonal };
+                return true;
+            }
+            if (AreSquaresMarked(antiDiag, CurrentPlayer))
+            {
+                winInfo = new WinInfo { Type = WinType.AntiDiagonal };
+                return true;
+            }
+            winInfo = null;
+            return false;
+        }
+        private bool DidMoveEndGame (int r,int c, out GameResult gameResult)
+        {
+
         }
     }
 }
